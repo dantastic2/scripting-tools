7e15331a53b88eed2b17987009a1ed47f7377203
Daniel Watson <dwatson@Daniels-MacBook-Air.local>
2026-01-18 23:40:37 -0500

    clean files off network drives

---
+++ ssh/cleanShield.sh
@@ -0,0 +1,24 @@
+open -g smb://beanphoner:carbon-bawd-boil@192.168.68.79/Seagate20A
+sleep 2
+open -g smb://beanphoner:carbon-bawd-boil@192.168.68.79/Seagate20B
+sleep 2
+open -g smb://beanphoner:carbon-bawd-boil@192.168.68.79/Seagate20C
+sleep 2
+open -g smb://beanphoner:carbon-bawd-boil@192.168.68.79/Seagate20D
+sleep 2
+open -g smb://beanphoner:carbon-bawd-boil@192.168.68.79/Seagate16UHD
+sleep 2
+open -g smb://beanphoner:carbon-bawd-boil@192.168.68.79/Seagate16TB
+sleep 2
+rm -rf /Volumes/Seagate20A/LOST.DIR/
+rm -rf /Volumes/Seagate20A/NVIDIA_SHIELD/
+rm -rf /Volumes/Seagate20B/LOST.DIR/
+rm -rf /Volumes/Seagate20B/NVIDIA_SHIELD/
+rm -rf /Volumes/Seagate20C/LOST.DIR/
+rm -rf /Volumes/Seagate20C/NVIDIA_SHIELD/
+rm -rf /Volumes/Seagate20D/LOST.DIR/
+rm -rf /Volumes/Seagate20D/NVIDIA_SHIELD/
+rm -rf /Volumes/Seagate16UHD/LOST.DIR/
+rm -rf /Volumes/Seagate16UHD/NVIDIA_SHIELD/
+rm -rf /Volumes/Seagate16TB/LOST.DIR/
+rm -rf /Volumes/Seagate16TB/NVIDIA_SHIELD/
