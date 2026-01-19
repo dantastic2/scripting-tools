1d1386c25d305df530b48a994970b2645224714c
Daniel Watson <dwatson@Daniels-MacBook-Air.local>
2026-01-18 23:33:07 -0500

    added HD remux too

--- ssh/volumes.sh
+++ ssh/volumes.sh
@@ -1,9 +1,13 @@
 
+#UHD Remux
 volumes=("T9 Black 1" "T9 Black 2" "T9 Black 3")
 volumes+=("T7 Black 1" "T7 Black 2" "T7 Black 3")
 volumes+=("T7 Grey 1" "T7 Grey 2" "T7 Grey 3" "T7 Grey 4")
 volumes+=("BlueSheild1" "BlueSheild2", "T7 White")
 volumes+=("CrucialX10")
+
+#HD Remux
+volumes+=("Orange" "Teal", "Sky Blue", "Crucial")
 
 
 # Loop through all items in the specified directory
@@ -12,7 +16,17 @@ for item in "/Volumes"/*; do
 	outputName="${volume// /}"
 	if [[ ${volumes[@]} =~ $volume ]] 
 		then 
-			echo $volume
-			find "/Volumes/$volume/UHD Remux/" -type f -exec basename {} \; | sort  | grep -v "._" > ~/movielist/ssd/$outputName.lst
+			echo $volume	
+			files=$(find "/Volumes/$volume/Movies/HD Remux/" -type f -exec basename {} \; 2>/dev/null | sort | grep -v "._")
+			files+=$(find "/Volumes/$volume/HD Remux/" -type f -exec basename {} \; 2>/dev/null | sort | grep -v "._")
+			if [ ${#files} -gt 0 ] 
+			then
+				echo $files  > ~/movielist/ssd/$outputName.hdremux.lst
+			fi
+			filesUHD=$(find "/Volumes/$volume/UHD Remux/" -type f -exec basename {} \; 2>/dev/null | sort | grep -v "._")
+			if [ ${#filesUHD} -gt 0 ] 
+			then
+				echo $filesUHD  > ~/movielist/ssd/$outputName.lst
+			fi
 	fi
 done
