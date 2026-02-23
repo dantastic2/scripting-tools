86ad6dc0f2d997a77540438aab5d7f74d1eb8c8f
Daniel Watson <dwatson@Daniels-MacBook-Air.local>
2026-02-23 13:46:49 -0500

    added t7 blue

--- bin/volumes.sh
+++ bin/volumes.sh
@@ -3,7 +3,7 @@
 volumes=("T9 Black 1" "T9 Black 2" "T9 Black 3")
 volumes+=("T7 Black 1" "T7 Black 2" "T7 Black 3")
 volumes+=("T7 Grey 1" "T7 Grey 2" "T7 Grey 3" "T7 Grey 4")
-volumes+=("BlueSheild1" "BlueSheild2", "T7 White")
+volumes+=("BlueSheild1" "BlueSheild2", "T7 White", "T7 Blue")
 volumes+=("CrucialX10")
 
 #HD Remux
