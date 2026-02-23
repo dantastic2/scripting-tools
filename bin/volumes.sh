a324fb3952586e9d04dd17ea5cdb65b787dd6525
Daniel Watson <dwatson@Daniels-MacBook-Air.local>
2026-02-23 13:53:13 -0500

    added more ssds

--- bin/volumes.sh
+++ bin/volumes.sh
@@ -3,8 +3,8 @@
 volumes=("T9 Black 1" "T9 Black 2" "T9 Black 3")
 volumes+=("T7 Black 1" "T7 Black 2" "T7 Black 3")
 volumes+=("T7 Grey 1" "T7 Grey 2" "T7 Grey 3" "T7 Grey 4")
-volumes+=("BlueSheild1" "BlueSheild2", "T7 White", "T7 Blue")
-volumes+=("CrucialX10")
+volumes+=("BlueSheild1" "BlueSheild2","T7 White", "T7 Blue")
+volumes+=("CrucialX10", "CrucialX9", "SanDisk2T")
 
 #HD Remux
 volumes+=("Orange" "Teal", "Sky Blue", "CrucialX9", "Sandisk 2TB")
